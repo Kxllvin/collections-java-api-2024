42f683d8583554d421bec31efe289f1a8fac56c7
Kelvin <kelvintenor987@gmail.com>
2024-07-16 13:39:31 -0300

    Modifiyng the class Aluno

--- src/main/set/Ordenacao/Aluno.java
+++ src/main/set/Ordenacao/Aluno.java
@@ -1,6 +1,10 @@
 package main.set.Ordenacao;
 
-public class Aluno {
+import java.util.Objects;
+import java.lang.Comparable;
+import java.util.Comparator;
+
+public class Aluno implements Comparable<Aluno> {
     //atributos
     private String nome;
     private long idAluno;
@@ -25,8 +29,34 @@ public class Aluno {
     }
 
     @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        if (this == obj) return true;
+        if(!(obj instanceof Aluno aluno)) return false;
+        return Objects.equals(getIdAluno(), aluno.getIdAluno());
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return Objects.hashCode(getIdAluno());
+    }
+
+    public int compareTo(Aluno aluno){
+        return nome.compareTo(aluno.getNome());
+    }
+
+    @Override
     public String toString() {
         // TODO Auto-generated method stub
         return "Aluno{" + "ID/Matr.:" + idAluno + " | Nome:" + nome + " | Nota:" + media +'}';
+    }
+}
+
+class ComparatorNota implements Comparator<Aluno>{
+    @Override
+    public int compare(Aluno o1, Aluno o2) {
+        // TODO Auto-generated method stub
+        return Double.compare(o1.getMedia(), o2.getMedia());
     }
 }
